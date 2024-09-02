f03c6de8d3e9758fc3f810e9608bb1483d0b5372
Lesinselva <lesinselva@gmail.com>
2024-09-02 14:41:33 +0530

    aa

--- lib/inventory.dart
+++ lib/inventory.dart
@@ -1,22 +1,21 @@
 library my_inventory_package;
 
 import 'package:flutter/material.dart';
-
 import 'package:net_store/mian.dart';
 import 'cos_container.dart';
 import 'custom_dialog.dart';
 
 class Inva extends StatefulWidget {
-  final IconData firstButtonIcon;
-  final String firstButtonText;
+  final IconData? firstButtonIcon;
+  final String? firstButtonText;
   final IconData secButtonIcon;
   final String secButtonText;
   final Color color;
 
   const Inva({
     super.key,
-    required this.firstButtonIcon,
-    required this.firstButtonText,
+    this.firstButtonIcon,
+    this.firstButtonText,
     required this.secButtonIcon,
     required this.secButtonText,
     required this.color,
@@ -142,65 +141,68 @@ class _InvaState extends State<Inva> {
             width: double.infinity,
             child: Row(
               children: [
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          final firstFieldController = TextEditingController();
-                          return CustomDialog(
-                            firstButtonIcon: Icons.check,
-                            secondButtonIcon: Icons.close,
-                            secondButtonAction: () {
-                              Navigator.of(context).pop();
-                            },
-                            icon: Icons.category,
-                            title: 'Add Category',
-                            subtitle: 'Category title',
-                            firstButtonLabel: 'Create',
-                            firstButtonColor:
-                                const Color.fromARGB(255, 39, 236, 22),
-                            firstButtonAction: (String title) {
-                              addCategoryContainer(title);
-                              Navigator.of(context).pop();
-                            },
-                            secondButtonLabel: 'Cancel',
-                            secondButtonColor: Colors.red,
-                            firstFieldController: firstFieldController,
-                            maxLength: 32,
-                            titleBackgroundGradient: const LinearGradient(
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter,
-                              colors: [
-                                Color.fromARGB(255, 251, 255, 26),
-                                Color.fromARGB(255, 136, 127, 1),
-                              ],
+                if (widget.firstButtonText != null &&
+                    widget.firstButtonIcon != null)
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            final firstFieldController =
+                                TextEditingController();
+                            return CustomDialog(
+                              firstButtonIcon: Icons.check,
+                              secondButtonIcon: Icons.close,
+                              secondButtonAction: () {
+                                Navigator.of(context).pop();
+                              },
+                              icon: Icons.category,
+                              title: 'Add Category',
+                              subtitle: 'Category title',
+                              firstButtonLabel: 'Create',
+                              firstButtonColor:
+                                  const Color.fromARGB(255, 39, 236, 22),
+                              firstButtonAction: (String title) {
+                                addCategoryContainer(title);
+                                Navigator.of(context).pop();
+                              },
+                              secondButtonLabel: 'Cancel',
+                              secondButtonColor: Colors.red,
+                              firstFieldController: firstFieldController,
+                              maxLength: 32,
+                              titleBackgroundGradient: const LinearGradient(
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                colors: [
+                                  Color.fromARGB(255, 251, 255, 26),
+                                  Color.fromARGB(255, 136, 127, 1),
+                                ],
+                              ),
+                            );
+                          },
+                        );
+                      },
+                      child: Container(
+                        height: 55,
+                        color: const Color.fromARGB(0, 255, 255, 255),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(widget.firstButtonIcon),
+                            Text(
+                              widget.firstButtonText!,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          );
-                        },
-                      );
-                    },
-                    child: Container(
-                      height: 55,
-                      color: const Color.fromARGB(0, 255, 255, 255),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(widget.firstButtonIcon),
-                          Text(
-                            widget.firstButtonText,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
                 Expanded(
                   child: GestureDetector(
                     onTap: () {
@@ -260,10 +262,7 @@ class _InvaState extends State<Inva> {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Icon(
-                            widget.secButtonIcon,
-                            color: Colors.white,
-                          ),
+                          Icon(widget.secButtonIcon, color: Colors.white),
                           Text(
                             widget.secButtonText,
                             style: const TextStyle(
